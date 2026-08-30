e1fe61e200d3fc8977ff15b3a3736ae3685ac817
DmitryKr2021 <dmitry.for.git@gmail.com>
2026-08-30 10:40:48 +0300

    fix password recovery

--- app/forms/loginReg.form.tsx
+++ app/forms/loginReg.form.tsx
@@ -216,7 +216,7 @@ export default function AuthModal() {
     setMessage(null);
 
     try {
-      const response = await fetch("/api/auth/forgot-password", {
+      const response = await fetch("/api/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
